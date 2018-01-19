4a5e5886dc931b2d0f0be5aa451549373accfa88
Tony Whomever <Soupedenuit@users.noreply.github.com>
2018-01-19 13:08:39 -0500

    Add files via upload

--- geckos6.js
+++ geckos6.js
@@ -1,16 +1,21 @@
-/*********************************
-**********************************
+/*******************************************
+********************************************
 **  AUTHOR:  Team Geckos-6
 ** PROJECT:  Chingu Voyage 3
 **    DATE:  Dec 2017-Jan 2018
 ** VERSION:  1.10
-**********************************
-*********************************/
+********************************************
+*******************************************/
 
-const geckos6AppModule = (function () {
+const geckos6AppModule = (function() {
 
   const cacheDom = {
     shift1: document.getElementsByClassName('shift1'),
+    shift2: document.getElementsByClassName('shift2'),
+    shift3: document.getElementsByClassName('shift3'),
+    shift4: document.getElementsByClassName('shift4'),
+    shift5: document.getElementsByClassName('shift5'),
+    shift6: document.getElementsByClassName('shift6'),
     shift7: document.getElementsByClassName('shift7'),
     line: document.getElementsByClassName('line'),
     schedule: document.getElementById('schedule'),
@@ -20,92 +25,16 @@ const geckos6AppModule = (function () {
     aside: document.getElementsByTagName('aside')[0],
     showMenu: document.getElementById('show-menu'),
     hideMenu: document.getElementById('hide-menu'),
-    settingsButtons: document.querySelectorAll('#settings > button')
+    settingsButtons: document.querySelectorAll('#settings > button'),
+    employeeObject: document.getElementById('employee-object'),
+    assignedShiftsObject: document.getElementById('assigned-shifts-object')
   };
 
-  // set width of shift7 bar (for testing only):
-  cacheDom.shift7[0].style.left = '25%';
-  cacheDom.shift7[0].style.width = '50%';
+  console.log('')
 
-  // set height of vertical lines:
-  let lines = cacheDom.line;
-  let scheduleHeight = cacheDom.schedule.getBoundingClientRect().height + 'px';
-  for ( let i = 0; i < lines.length; i++ ) {
-    lines[i].style.height = scheduleHeight;
-  }
-
-  // Adds text to horizontal "shift" bars:
-  (function nameShift1() {
-    let lgt = cacheDom.shift1.length;
-    for ( let i = 0; i < lgt; i++ ) {
-      cacheDom.shift1[i].innerText = 'employee name 7:00am to 1:45pm';
-    }
-  })()
-
-  const hideMenu = function() {
-    cacheDom.aside.classList.add('hide');
-    cacheDom.aside.classList.add('hide2');
-  }
-
-  const showMenu = function() {
-    cacheDom.aside.classList.remove('hide');
-    cacheDom.aside.classList.remove('hide2');
-  }
-
-  cacheDom.showMenu.addEventListener('click', showMenu, false);
-
-  cacheDom.hideMenu.addEventListener('click', hideMenu, false);
-
-  const placeholderValue = function(event) {
-    if ( event.target !== event.currentTarget ) {
-      if ( event.target.nodeName === 'BUTTON') {
-        let text = event.target.innerText + `...`;
-        cacheDom.settingsInput.setAttribute('placeholder', text);
-        console.log(event.target.nodeName);
-      }
-    }
-  };
-
-  cacheDom.settings.addEventListener('click', placeholderValue, false);
-
-  // Navigation button by David:
-  // Currently unused
-
-  // var display = false;
-  // $(document).ready(function() {
-  //   $("#navigationButton").click(function() {
-  //     if(!display) {
-  //       $("ul li").css("display", "block");
-  //       display = true;
-  //     }
-  //     else {
-  //       $("ul li").css("display", "none");
-  //       display = false;
-  //     }
-  //   });
-  // });
-
-
-  /*********************************
-   Input Objects from settings:
-  *********************************/
-
-  // functions for pulling data from settings menu:
-  const createEmployees = function(name, maxHours, avail) {
-      let employeeName = name;
-      employees[employeeName] = { maxHours, avail };
-    };
-
-  const createTimetable = function(day, open, close, staffRequired) {
-    timetable[day][open] = open;
-    timetable[day][close] = close;
-    timetable[day][staffRequired] = staffRequired;
-  };
-
-
-  /*********************************
+  /*****************************************
    Schedule Maker algorithm:
-  *********************************/
+  *****************************************/
 
   // variables setup:
   const employeeArr1 = [];
@@ -117,115 +46,8 @@ const geckos6AppModule = (function () {
   var assignedShiftsObj2 = {};
 
   // Sample employees object for testing:
-  const employees = {
-    'Eddard Stark': {
-      maxHours: 12,
-      avail: [1, 2, 5, 7]
-    },
-    'Robert Baratheon': {
-      maxHours: 32,
-      avail: [2, 4, 5]
-    },
-    'Jaime Lannister': {
-      maxHours: 8,
-      avail: [1, 2, 5, 6]
-    },
-    'Catelyn Stark': {
-      maxHours: 16,
-      avail: [2, 3, 4, 5]
-    },
-    'Cersei Lannister': {
-      maxHours: 4,
-      avail: [1, 3, 5, 7]
-    },
-    'Daenerys Targaryen': {
-      maxHours: 24,
-      avail: [2, 4, 5, 6, 7]
-    },
-    'Jorah Mormont': {
-      maxHours: 20,
-      avail: [2, 4, 6, 7]
-    },
-    'Petyr Baelish': {
-      maxHours: 12,
-      avail: [2, 4, 5]
-    },
-    'Viserys Targaryen': {
-      maxHours: 44,
-      avail: [1, 3, 4, 5, 6, 7]
-    },
-    'Jon Snow': {
-      maxHours: 8,
-      avail: [2, 4, 6]
-    },
-    'Sansa Stark': {
-      maxHours: 16,
-      avail: [2, 4, 7]
-    },
-    'Arya Stark': {
-      maxHours: 24,
-      avail: [1, 2, 4, 5]
-    },
-    'Robb Stark': {
-      maxHours: 4,
-      avail: [3, 5, 6]
-    },
-    'Theon Greyjoy': {
-      maxHours: 12,
-      avail: [3, 4, 5, 6]
-    },
-    'Bran Stark': {
-      maxHours: 16,
-      avail: [1, 2, 3, 5, 7]
-    },
-    'Joffrey Baratheon': {
-      maxHours: 24,
-      avail: [3, 4, 5, 7]
-    },
-    'Tyrion Lannister': {
-      maxHours: 28,
-      avail: [1, 2, 3, 4, 6]
-    }
-  };
+  // Moved to an external file named 'myObjects'
 
-  // Sample timetable object for testing:
-  const timetable = {
-    Monday: {
-      open: 7,
-      close: 17,
-      staffRequired: 2
-    },
-    Tuesday: {
-      open: 7,
-      close: 17,
-      staffRequired: 3
-    },
-    Wednesday: {
-      open: 7,
-      close: 18,
-      staffRequired: 2
-    },
-    Thursday: {
-      open: 7,
-      close: 20,
-      staffRequired: 3
-    },
-    Friday: {
-      open: 7,
-      close: 20,
-      staffRequired: 3
-    },
-    Saturday: {
-      open: 7,
-      close: 20,
-      staffRequired: 5
-    },
-    Sunday: {
-      open: 7,
-      close: 16,
-      staffRequired: 4
-    }
-  };
 
   // create initial array of employee names with a repeat for every number of possible shifts:
   // identifier: 00-00-00-0-name-[avail]
@@ -289,12 +111,14 @@ const geckos6AppModule = (function () {
   };
 
   // create initial array of shift slots (creates sub-arrays):
-  const shiftsArr1Maker = function() {
+  const shiftsArr1Maker = function(nameNum) {
     let weekdays = Object.keys(timetable);
     // console.log(weekdays);
     weekdays.forEach(function(weekday, index) {
       // console.log(x, 'open: ', timetable[x].open, 'close: ', timetable[x].close);
       // let weekday = timetable[x];
+      let day = weekday;
+      nameNum === 'num' ? day = index + 1 : null;
       let open = timetable[weekday].open;
       let close = timetable[weekday].close;
       let staffRequired = timetable[weekday].staffRequired;
@@ -304,31 +128,31 @@ const geckos6AppModule = (function () {
       let timeSlots = [];
       var timeSlot1, timeSlot2, timeSlot3, timeSlot4;
       if ( dayLength < 8 ) {
-        timeSlot1 = `${index + 1}-${weekday}: ${start} to ${close}`;
+        timeSlot1 = `${index + 1}-${day} ${start} to ${close}`;
         for ( let i = 0; i < staffRequired; i++ ) {
           timeSlots.push(timeSlot1);
         }
       }
       else if ( dayLength >=8 && dayLength < 12 ) {
-        timeSlot1 = `${index + 1}-${weekday}: ${start} to ${start+4}`;
-        timeSlot2 = `${index + 1}-${weekday}: ${start+4} to ${close}`;
+        timeSlot1 = `${index + 1}-${day} ${start} to ${start+4}`;
+        timeSlot2 = `${index + 1}-${day} ${start+4} to ${close}`;
         for ( let i = 0; i < staffRequired; i++ ) {
           timeSlots.push(timeSlot1, timeSlot2);
         }
       }
       else if ( dayLength >=12 && dayLength < 16 ) {
-        timeSlot1 = `${index + 1}-${weekday}: ${start} to ${start+4}`;
-        timeSlot2 = `${index + 1}-${weekday}: ${start+4} to ${start+8}`;
-        timeSlot3 = `${index + 1}-${weekday}: ${start+8} to ${close}`;
+        timeSlot1 = `${index + 1}-${day} ${start} to ${start+4}`;
+        timeSlot2 = `${index + 1}-${day} ${start+4} to ${start+8}`;
+        timeSlot3 = `${index + 1}-${day} ${start+8} to ${close}`;
         for ( let i = 0; i < staffRequired; i++ ) {
           timeSlots.push(timeSlot1, timeSlot2, timeSlot3);
         }
       }
       else if ( dayLength >=16 && dayLength < 20 ) {
-        timeSlot1 = `${index + 1}-${weekday}: ${start} to ${start+4}`;
-        timeSlot2 = `${index + 1}-${weekday}: ${start+4} to ${start+8}`;
-        timeSlot3 = `${index + 1}-${weekday}: ${start+8} to ${start+12}`;
-        timeSlot4 = `${index + 1}-${weekday}: ${start+12} to ${close}`;
+        timeSlot1 = `${index + 1}-${day} ${start} to ${start+4}`;
+        timeSlot2 = `${index + 1}-${day} ${start+4} to ${start+8}`;
+        timeSlot3 = `${index + 1}-${day} ${start+8} to ${start+12}`;
+        timeSlot4 = `${index + 1}-${day} ${start+12} to ${close}`;
         for ( let i = 0; i < staffRequired; i++ ) {
           timeSlots.push(timeSlot1, timeSlot2, timeSlot3, timeSlot4);
         }
@@ -405,16 +229,36 @@ const geckos6AppModule = (function () {
       let j = i.toString().padStart(2, '0');
       if ( edit === 'edit' ) {
         empl = empl.match(/[A-Za-z]+/g).join(' ');
-        shift = shift.substring(2, shift.length-1);
+        shift = shift.substring(2, shift.length);
       }
-      assignedShiftsObj2[j + '. ' + shift] = empl;
+      assignedShiftsObj2[j + ' ' + shift] = empl;
       let remove2 = employeeArr3.indexOf(empl);
       employeeArr3.splice(remove1, 1);
     }
   };
 
 
-  // run all functions:
+  /*****************************************
+   Create Objects from menu settings:
+  *****************************************/
+
+  // functions for pulling data from settings menu:
+  const createEmployees = function(name, maxHours, avail) {
+      let employeeName = name;
+      employees[employeeName] = { maxHours, avail };
+    };
+
+  const createTimetable = function(day, open, close, staffRequired) {
+    timetable[day][open] = open;
+    timetable[day][close] = close;
+    timetable[day][staffRequired] = staffRequired;
+  };
+
+
+  /*****************************************
+   run all scheduling functions:
+  *****************************************/
+
   const createAll = (function() {
     employeeArr1Maker(); //repl.it indicating 'too many errors' but it's working fine!
     employeeArr2Maker();
@@ -423,6 +267,160 @@ const geckos6AppModule = (function () {
     shiftsArr2Maker();
     assignedShiftsObj1Maker();
     assignedShiftsObj2Maker('edit'); // cleanup result
+    // createEmployees();
+    // createTimetable();
   })();
 
+
+  /*****************************************
+   Display employee object to menu page:
+  *****************************************/
+
+  // var output = '';
+  // for ( var property in employees ) {
+  //   output += property + ': ' + employees[property] +'; ';
+  // }
+  // or
+  // cacheDom.employeeObject.innerText = JSON.stringify(employees); //output;
+
+  let text1 = `<h3>Employee object</h3><br/>`;
+  Object.keys(employees).forEach(function(name) {
+    let maxHours = employees[name].maxHours;
+    let avail = employees[name].avail;
+    text1 += `<ins>${name}</ins>: &nbsp&nbsp max hours: ${maxHours} /  avail: ${avail}</br>`;
+  });
+  cacheDom.employeeObject.innerHTML = text1;
+
+
+  /*****************************************
+   Display assigned shifts object to menu page:
+  *****************************************/
+
+  let text2 = `<h3>Assigned shifts</h3><br/>`;
+  for ( var shift in assignedShiftsObj2 ) {
+    let name = assignedShiftsObj2[shift];
+    text2 += `<ins>${shift}</ins>: &nbsp&nbsp ${name}</br>`;
+  };
+  cacheDom.assignedShiftsObject.innerHTML = text2;
+
+
+  /*****************************************
+   Distribute shifts on schedule grid
+   by manipulating shift bars from
+   info extracted from assignedShiftsObj2:
+  *****************************************/
+
+  function nameShift1() {
+    let lgt = cacheDom.shift1.length;
+    let obj = assignedShiftsObj2;
+    let employee = Object.values(obj)[1];
+    let start = Object.keys(obj)[1].split(' ')[2];
+    let end = Object.keys(obj)[1].split(' ')[4];
+    let dayLength = 14;
+    let barLeft = Math.round((start - 7 + 0.01) / 14) * 100;
+    let barWidth = (end - start + 0.01) / 14 * 100;
+    cacheDom.shift1[0].style.left = barLeft + '%';
+    cacheDom.shift1[0].style.width = barWidth + '%';
+    for ( let i = 0; i < lgt; i++ ) {
+      cacheDom.shift1[i].textContent = `${employee}:  ${start} to ${end}`;
+    }
+  }
+
+  function nameShift2() { //ZZ
+    let lgt = cacheDom.shift2.length;
+    let obj = assignedShiftsObj2;
+    let employee = Object.values(obj)[2];
+    let start = Object.keys(obj)[2].split(' ')[2];
+    let end = Object.keys(obj)[2].split(' ')[4];
+    let dayLength = 14;
+    let barLeft = Math.round((start - 7 + 0.01) / 14) * 100;
+    let barWidth = (end - start + 0.01) / 14 * 100;
+    cacheDom.shift2[0].style.left = barLeft + '%';
+    cacheDom.shift2[0].style.width = barWidth + '%';
+    for ( let i = 0; i < lgt; i++ ) {
+      cacheDom.shift2[i].textContent = `${employee}:  ${start} to ${end}`;
+    }
+  }
+
+  nameShift1();
+  nameShift2();
+
+  // manipulate shift1 bar (testing);
+
+  // set width of shift1 bar (for testing only):
+  // cacheDom.shift1[0].style.left = '1%';
+  // cacheDom.shift1[0].style.width = '50%';
+
+
+  /*****************************************
+   Set height of vertical lines (CSS manip)
+  *****************************************/
+
+  let lines = cacheDom.line;
+  let scheduleHeight = cacheDom.schedule.getBoundingClientRect().height + 'px';
+  for ( let i = 0; i < lines.length; i++ ) {
+    lines[i].style.height = scheduleHeight;
+  }
+
+
+  /*****************************************
+   Navigation functions:
+  *****************************************/
+
+  const hideMenu = function() {
+    cacheDom.aside.classList.add('hide');
+    cacheDom.aside.classList.add('hide2');
+  }
+
+  const showMenu = function() {
+    cacheDom.aside.classList.remove('hide');
+    cacheDom.aside.classList.remove('hide2');
+  }
+
+  cacheDom.showMenu.addEventListener('click', showMenu, false);
+
+  cacheDom.hideMenu.addEventListener('click', hideMenu, false);
+
+  // const placeholderValue = function(event) {
+  //   if ( event.target !== event.currentTarget ) {
+  //     if ( event.target.nodeName === 'BUTTON') {
+  //       let text = event.target.innerText + `...`;
+  //       cacheDom.settingsInput.setAttribute('placeholder', text);
+  //       console.log(event.target.nodeName);
+  //     }
+  //   }
+  // };
+
+  // cacheDom.settings.addEventListener('click', placeholderValue, false);
+
+
+  /*****************************************
+   Navigation button by David:
+   (currently unused)
+  *****************************************/
+
+  // var display = false;
+  // $(document).ready(function() {
+  //   $("#navigationButton").click(function() {
+  //     if(!display) {
+  //       $("ul li").css("display", "block");
+  //       display = true;
+  //     }
+  //     else {
+  //       $("ul li").css("display", "none");
+  //       display = false;
+  //     }
+  //   });
+  // });
+
+
 })();
+
+/*******************************************
+********************************************
+**  AUTHOR:  Team Geckos-6
+** PROJECT:  Chingu Voyage 3
+**    DATE:  Dec 2017-Jan 2018
+** VERSION:  1.10
+********************************************
+*******************************************/
